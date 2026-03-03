668aa1f1ec8454f8fece6ade02539ef964418fd9
汪承睿 <15753478+wang-chengrui12138@user.noreply.gitee.com>
2026-03-03 15:01:08 +0800

    qianwen api vercel fix 1501

--- api/qianwen.js
+++ api/qianwen.js
@@ -1,6 +1,6 @@
-// ✅ 使用标准 CommonJS 导出
+// Vercel Serverless Function for Qianwen API
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
     // 设置CORS头
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -86,4 +86,4 @@ module.exports = async (req, res) => {
             message: error.message
         });
     }
-};
+}
